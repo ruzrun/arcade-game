7c1ab45158aba2ae899443f328cd1f475132820d
ruzrun <warunwan1314@gmail.com>
2026-07-24 18:55:10 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function openGame(game) {
+    if (game === "snake") {
+        window.location.href = "games/snake/";
+    }
+}
+
+function backToLetters() {
+    window.location.href = "https://YOUR-PRECIOUS-LETTER-WEBSITE.github.io";
+}
